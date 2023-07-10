aa5b87d4120270ec35105df67dcfcb2781ab0ec8
Joshua Dean <dea16006@byui.edu>
2023-07-09 23:17:37 -0600

    User.js internal server bugfix-attempt

--- routes/users.js
+++ routes/users.js
@@ -21,6 +21,7 @@ router.get('/callback', async (req, res) => {
       existingUser.name = name;
       existingUser.email = email;
       existingUser.picture = picture;
+      console.log('I got here');
 
       await existingUser.save();
     } else {
@@ -32,8 +33,10 @@ router.get('/callback', async (req, res) => {
         email,
         picture,
       });
+      console.log('Nah I got here');
     }
 
+    console.log('Actually I got here');
     res.redirect('/profile');
   } catch (error) {
     res.status(500).json({ message: 'Server error' });
